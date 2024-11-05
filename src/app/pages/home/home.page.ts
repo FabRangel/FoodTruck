39b38a98be9564970064210f52501abc7c7e7671
Fabiola Martínez Rangel <fgmr.rangel@gmail.com>
2024-11-04 23:11:28 -0600

    Items foods with icons

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonAvatar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonAvatar, IonButtons, IonButton, IonIcon, IonItemOption, IonItemOptions, IonItemSliding } from '@ionic/angular/standalone';
 import { FoodService } from 'src/app/services/food.service';
+import { addIcons } from 'ionicons';
+import { pencilOutline, trashOutline, createOutline,trash,create } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonAvatar, IonItem, IonLabel, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonItemSliding, IonItemOptions, IonItemOption, IonIcon, IonButton, IonButtons, IonAvatar, IonItem, IonLabel, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
   foods:any[] = [];
@@ -19,6 +21,7 @@ export class HomePage implements OnInit {
       console.log(res);
       this.foods = res;
     });
+    addIcons({create,trash,createOutline,trashOutline,pencilOutline});
    }
 
   ngOnInit() {
